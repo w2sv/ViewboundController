5cf335a69c2b016b663c487be0ce480f09a2a6f2
w2sv <zangenbergjanek@googlemail.com>
2022-10-23 15:56:09 +0200

    Add Activity test

---
+++ lib/src/androidTest/java/com/w2sv/viewboundcontroller/ViewBoundActivityTest.kt
@@ -0,0 +1,25 @@
+package com.w2sv.viewboundcontroller
+
+import bindings.ActivityTestBinding
+import de.mannodermaus.junit5.ActivityScenarioExtension
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class ExampleViewBoundActivity: ViewBoundActivity<ActivityTestBinding>(
+    ActivityTestBinding::class.java)
+
+internal class ViewBoundActivityTest{
+    @JvmField
+    @RegisterExtension
+    val scenarioExtension = ActivityScenarioExtension.launch<ExampleViewBoundActivity>()
+
+    @Test
+    fun bindingPropertiesAccessible(){
+        scenarioExtension.scenario.onActivity {
+            Assertions.assertEquals(-1, it.binding.root.id)
+            Assertions.assertEquals(2131230800, it.binding.dummyTv.id)
+            Assertions.assertEquals(2131230799, it.binding.dummyIv.id)
+        }
+    }
+}
